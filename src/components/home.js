fe13906d58deb528486323d7eef68873aeba30f8
Badru Ndawula <badrundawula24@gmail.com>
2023-09-28 16:35:43 +0300

    add home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,27 @@
-const Home = () => (
-  <div className="home">
-    <h1>Welcome to my page</h1>
-    <p className="p1">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi cumque
+import React from 'react';
+import './style.css';
+
+function Home() {
+  return (
+    <>
+      <div className="home-cont">
+        <h2>Weclome to our page!</h2>
+        <p className="par">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi cumque
       magni placeat repellendus dolorem, enim nesciunt incidunt distinctio sunt
       corporis sed, quo, laborum eaque nam exercitationem ducimus nisi quas
       saepe!
-    </p>
-
-    <p className="p2">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolore
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolore
       provident sequi, dolorum repellendus culpa incidunt dicta nobis vero,
       debitis non excepturi voluptas? Exercitationem alias, vel esse rerum
       facere libero.
-    </p>
-  </div>
-);
+        </p>
+      </div>
+    </>
+  );
+}
 
 export default Home;
+
