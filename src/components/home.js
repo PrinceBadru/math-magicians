23741301d39e9e2d1815e1b0cceceb067ec34485
Badru Ndawula <badrundawula24@gmail.com>
2023-09-28 16:48:33 +0300

    add linters fix

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 function Home() {
   return (
@@ -7,16 +7,16 @@ function Home() {
       <div className="home-cont">
         <h2>Weclome to our page!</h2>
         <p className="par">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi cumque
-      magni placeat repellendus dolorem, enim nesciunt incidunt distinctio sunt
-      corporis sed, quo, laborum eaque nam exercitationem ducimus nisi quas
-      saepe!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+          cumque magni placeat repellendus dolorem, enim nesciunt incidunt
+          distinctio sunt corporis sed, quo, laborum eaque nam exercitationem
+          ducimus nisi quas saepe!
           <br />
           <br />
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolore
-      provident sequi, dolorum repellendus culpa incidunt dicta nobis vero,
-      debitis non excepturi voluptas? Exercitationem alias, vel esse rerum
-      facere libero.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          dolore provident sequi, dolorum repellendus culpa incidunt dicta nobis
+          vero, debitis non excepturi voluptas? Exercitationem alias, vel esse
+          rerum facere libero.
         </p>
       </div>
     </>
@@ -24,4 +24,3 @@ function Home() {
 }
 
 export default Home;
-
