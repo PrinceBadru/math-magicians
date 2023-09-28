7d3fab658983465e2dfd94f2c669675edb1cdc67
Badru Ndawula <badrundawula24@gmail.com>
2023-09-28 09:04:18 +0300

    add home.js

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+const Home = () => (
+  <div className="home">
+    <h1>Welcome to my page</h1>
+    <p className="p1">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi cumque
+      magni placeat repellendus dolorem, enim nesciunt incidunt distinctio sunt
+      corporis sed, quo, laborum eaque nam exercitationem ducimus nisi quas
+      saepe!
+    </p>
+
+    <p className="p2">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolore
+      provident sequi, dolorum repellendus culpa incidunt dicta nobis vero,
+      debitis non excepturi voluptas? Exercitationem alias, vel esse rerum
+      facere libero.
+    </p>
+  </div>
+);
+
+export default Home;
